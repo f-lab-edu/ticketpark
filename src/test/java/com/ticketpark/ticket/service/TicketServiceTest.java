7b14ad9c2330cad2fbd0676e477c5f9a0a20c238
skdev0619 <skdev0619@naver.com>
2024-08-01 17:30:48 +0900

    기존 테스트케이스 롤백테스트 지원하도록 변경

--- src/test/java/com/ticketpark/ticket/service/TicketServiceTest.java
+++ src/test/java/com/ticketpark/ticket/service/TicketServiceTest.java
@@ -1,24 +1,19 @@
 package com.ticketpark.ticket.service;
 
 import com.ticketpark.ticket.model.dto.TicketCreateDto;
-import com.ticketpark.ticket.model.dto.TicketDto;
-import com.ticketpark.ticket.model.dto.TicketGradeDto;
-import com.ticketpark.ticket.service.fixture.TicketFixture;
-import com.ticketpark.ticket.service.fixture.TicketGradeFixture;
-import org.junit.jupiter.api.Assertions;
+import com.ticketpark.ticket.fixture.TicketFixture;
+import com.ticketpark.ticket.fixture.TicketGradeFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.IllegalTransactionStateException;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
+@Transactional
 public class TicketServiceTest {
 
     @Autowired
@@ -34,8 +29,7 @@ public class TicketServiceTest {
     @Test
     @DisplayName("티켓은 공연과 같이 등록되어야 한다")
     void addTicketWithoutPerformance(){
-        //티켓만 insert 시 트랜잭션 전파 옵션으로 인한 exception 발생
-        Assertions.assertThrows(IllegalTransactionStateException.class, () -> ticketService.createTicket(request));
+        assertThrows(IllegalTransactionStateException.class, () -> ticketService.createTicket(request));
     }
 
 }

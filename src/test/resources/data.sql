84fe902206759dec15936cf22be1450e26a70079
skdev0619 <skdev0619@naver.com>
2024-08-02 15:10:47 +0900

    테스트코드 초기 데이터 sql h2 문법 적용

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -4,13 +4,13 @@ values('test', 'U', '1q2w3e4r', 'aa@email.com', 01012345678, now(), now());
 
 SET @fix_date = '2024-08-01 18:00:00';
 insert into performance (genre, name, place, start_dt, end_dt, created_at)
-values('CO', '2024 SM 통합콘서트', '고척돔', date_add(@fix_date , INTERVAL 10 DAY), date_add(@fix_date , INTERVAL 12 DAY), NOW());
+values('CO', '2024 SM 통합콘서트', '고척돔', TIMESTAMPADD(DAY, 10, @fix_date), TIMESTAMPADD(DAY, 12, @fix_date), NOW());
 
 insert into performer(name, performance_id)
 values('에스파', 1), ('nct', 1), ('라이즈', 1);
 
 insert into ticket (start_dt, end_dt, created_dt, performance_id)
-values(date_add(@fix_date , INTERVAL 3 DAY),date_add(@fix_date , INTERVAL 10 DAY), now(), 1);
+values(TIMESTAMPADD(DAY, 3, @fix_date),TIMESTAMPADD(DAY, 10, @fix_date), now(), 1);
 
 insert into ticket_grade(grade, grade_name, seat_count, price, created_dt, ticket_id)
 values ('VIP', 'VIP석', 100, 200000, NOW(), 1)

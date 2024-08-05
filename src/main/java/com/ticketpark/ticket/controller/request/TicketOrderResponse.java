015083be6f38330a87ab6fb531d1fe4cfcdbccd3
skdev0619 <skdev0619@naver.com>
2024-08-05 16:06:31 +0900

    불필요한 @Builder 제거

--- src/main/java/com/ticketpark/ticket/controller/request/TicketOrderResponse.java
+++ src/main/java/com/ticketpark/ticket/controller/request/TicketOrderResponse.java
@@ -5,23 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-@Builder
 @Getter
 @AllArgsConstructor
 public class TicketOrderResponse {
-    private Long ticket_order_id;
-    private Long member_id;
-    private Long performance_id;
-    private Long ticket_grade_id;
-    private String seat_info;
+    private Long ticketOrderId;
+    private Long memberId;
+    private Long performanceId;
+    private Long ticketGradeId;
+    private String seatInfo;
 
     public static TicketOrderResponse fromTicketOrder(TicketOrder ticketOrder) {
-        return TicketOrderResponse.builder()
-                .ticket_order_id(ticketOrder.getTicket_order_id())
-                .member_id(ticketOrder.getMember_id())
-                .performance_id(ticketOrder.getPerformance_id())
-                .ticket_grade_id(ticketOrder.getTicket_grade_id())
-                .seat_info(ticketOrder.getSeat_info())
-                .build();
+        return new TicketOrderResponse(
+                ticketOrder.getTicket_order_id()
+                , ticketOrder.getMember_id()
+                , ticketOrder.getPerformance_id()
+                , ticketOrder.getTicket_grade_id()
+                , ticketOrder.getSeat_info()
+        );
     }
 }

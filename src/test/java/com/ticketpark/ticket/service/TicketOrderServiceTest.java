f968de583e2e81ce897973ab16ee8a63a56fc717
skdev0619 <skdev0619@naver.com>
2024-08-19 17:41:50 +0900

    트랜잭션 전파 exception 테스트 오류 수정

--- src/test/java/com/ticketpark/ticket/service/TicketOrderServiceTest.java
+++ src/test/java/com/ticketpark/ticket/service/TicketOrderServiceTest.java
@@ -3,16 +3,17 @@ package com.ticketpark.ticket.service;
 import com.ticketpark.exception.TicketParkException;
 import com.ticketpark.ticket.fixture.TicketOrderFixture;
 import com.ticketpark.ticket.model.dto.TicketOrderDto;
+import com.ticketpark.ticket.repository.TicketGradeRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.IllegalTransactionStateException;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
 public class TicketOrderServiceTest {
 
     @Autowired
@@ -23,7 +24,7 @@ public class TicketOrderServiceTest {
 
     private TicketOrderDto ticketOrderDto;
 
-
+    @Transactional
     @DisplayName("예매 안된 좌석 예매하면 예매 가능")
     @Test
     void bookTicketPossible(){
@@ -40,11 +41,12 @@ public class TicketOrderServiceTest {
         assertDoesNotThrow(()->ticketOrderService.checkBookableTicket(ticketOrderDto));
     }
 
+    @Transactional
     @DisplayName("이미 예약된 좌석을 예매하면 예매 불가능")
     @Test
     void duplicateBookTicket(){
         //given
-        ticketOrderDto = TicketOrderFixture.getTicketOrderDto("1층-A구역-1열-001");
+        ticketOrderDto = TicketOrderFixture.getTicketOrderDto("1층-B구역-1열-001");
 
         //then
         //1층-A구역-1열-001 예매
@@ -52,15 +54,14 @@ public class TicketOrderServiceTest {
 
         //when
         //동일 좌석 예매 가능한지 확인
-        ticketOrderDto = TicketOrderFixture.getTicketOrderDto("1층-A구역-1열-001");
+        ticketOrderDto = TicketOrderFixture.getTicketOrderDto("1층-B구역-1열-001");
         assertThrows(TicketParkException.class, ()-> ticketOrderService.checkBookableTicket(ticketOrderDto));
     }
 
 
-    @DisplayName("티켓 예매 정보 저장")
+    @DisplayName("티켓 수/티켓 중복 예매 유효성 체크 없이 예매하면 트랜잭션 전파 exception 발생")
     @Test
     void saveTicketOrder(){
-        ticketOrderDto = TicketOrderFixture.getTicketOrderDto();
-        assertDoesNotThrow(()->ticketOrderService.saveTicketOrder(ticketOrderDto));
+        assertThrows(IllegalTransactionStateException.class,()->ticketOrderService.saveTicketOrder(ticketOrderDto));
     }
 }
--- src/test/java/com/ticketpark/ticket/service/TicketServiceTest.java
+++ src/test/java/com/ticketpark/ticket/service/TicketServiceTest.java
@@ -9,11 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.IllegalTransactionStateException;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
-@Transactional
 public class TicketServiceTest {
 
     @Autowired

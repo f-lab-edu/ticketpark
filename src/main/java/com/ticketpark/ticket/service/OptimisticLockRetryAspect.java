78401a61be670537ee6e98deef77f760200b4d3b
skdev0619 <skdev0619@naver.com>
2024-08-19 18:17:51 +0900

    낙관적 락 적용

---
+++ src/main/java/com/ticketpark/ticket/service/OptimisticLockRetryAspect.java
@@ -0,0 +1,35 @@
+package com.ticketpark.ticket.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class OptimisticLockRetryAspect {
+
+    @Value("${retry.optimisticLock.count}")
+    private byte retryCount;
+
+    @Around("@annotation(retry)")
+    public Object doRetry(ProceedingJoinPoint joinPoint, OptimisticLockRetry retry) throws Throwable {
+        RuntimeException exceptionHolder = null;
+        int maxRetry = retryCount > 0 ? retryCount : retry.value();
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.debug("[retry] try count {}/{}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            } catch (OptimisticLockingFailureException e) {
+                log.debug("버전 충돌 발생 / 예매 retry start {}", joinPoint.getArgs());
+                exceptionHolder = e;
+            }
+        }
+        throw exceptionHolder;
+    }
+}

64e78cae75258b7488d641e7a0061e5188e9b9f7
skdev0619 <skdev0619@naver.com>
2024-08-30 15:45:47 +0900

    락 구분에 의해 불필요하게 생겼던 파일들 제거

---
+++ src/main/java/com/ticketpark/ticket/service/TicketGradeServiceByNamedLock_20240830_lsk.java
@@ -0,0 +1,27 @@
+//
+//package com.ticketpark.ticket.service;
+//
+//import com.ticketpark.exception.ErrorCode;
+//import com.ticketpark.exception.TicketParkException;
+//import com.ticketpark.ticket.model.dto.TicketOrderDto;
+//import com.ticketpark.ticket.repository.TicketGradeRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.transaction.annotation.Transactional;
+//
+////@Service
+//@RequiredArgsConstructor
+//public class TicketGradeServiceByNamedLock implements TicketGradeService {
+//    private final TicketGradeRepository ticketGradeRepository;
+//
+//    @Transactional(readOnly = true)
+//    public boolean isFullBooked(TicketOrderDto dto){
+//        //해당 좌석 등급의 남아있는 티켓 매수 확인
+//        Integer remainTicketCount = ticketGradeRepository.getCountTicketGrade(dto.getTicket_grade_id());
+//        if(remainTicketCount<=0){
+//            throw new TicketParkException(ErrorCode.ALL_BOOKED_TICKET
+//                    , String.format("this ticket grade is all booked / ticket_grade_id : %s", dto.getTicket_grade_id()));
+//        }
+//        return true;
+//    }
+//}
+//
---
+++ src/main/java/com/ticketpark/ticket/service/TicketOrderService_20240830_lsk.java
@@ -0,0 +1,17 @@
+//package com.ticketpark.ticket.service;
+//
+//import com.ticketpark.exception.ErrorCode;
+//import com.ticketpark.exception.TicketParkException;
+//import com.ticketpark.ticket.model.dto.TicketOrderDto;
+//import com.ticketpark.ticket.model.entity.TicketOrder;
+//import com.ticketpark.ticket.repository.TicketGradeRepository;
+//import com.ticketpark.ticket.repository.TicketOrderRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Propagation;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//public interface TicketOrderService {
+//    public void checkBookableTicket(TicketOrderDto orderDto);
+//    public TicketOrder saveTicketOrder(TicketOrderDto orderDto);
+//}
